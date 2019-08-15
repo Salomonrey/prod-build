da7685113039df6f27d58fdb3d9ff52232129b3b
Salomonrey <36483352+Salomonrey@users.noreply.github.com>
2019-08-15 18:23:32 +0600

    Sula

--- client/app.js
+++ client/app.js
@@ -1,9 +1,20 @@
 const path = require('path');
 const compression = require("compression");
 const express = require('express');
+var fs = require('fs');
 const app = express();
 app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/sitemap.xml', function(req, res) {
+    res.header('Content-Type', 'application/xml');
+    res.send( fs.readFileSync(require('path').resolve(__dirname,'./sitemap.xml'), "utf8") );
+});
+app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+});
 app.get('/*', function(req, res) {
  // res.setHeader('Content-Type', 'text/event-stream')
  // res.setHeader('Cache-Control', 'no-cache')
