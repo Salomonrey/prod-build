9f031ff24eabfb254dc2e1f5b8db292cfbf20b57
Salomonrey <36483352+Salomonrey@users.noreply.github.com>
2019-08-15 18:31:45 +0600

    Sula

--- client/app.js
+++ client/app.js
@@ -11,10 +11,10 @@ app.get('/sitemap.xml', function(req, res) {
     res.header('Content-Type', 'application/xml');
     res.send( fs.readFileSync(require('path').resolve(__dirname,'./sitemap.xml'), "utf8") );
 });
-app.get('/robots.txt', function (req, res) {
-    res.type('text/plain');
-    res.send("User-agent: *\nDisallow: /");
-});
+// app.get('/robots.txt', function (req, res) {
+//     res.type('text/plain');
+//     res.send("User-agent: *\nDisallow: /");
+// });
 app.get('/*', function(req, res) {
  // res.setHeader('Content-Type', 'text/event-stream')
  // res.setHeader('Cache-Control', 'no-cache')
