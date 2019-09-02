4d7d8f1f63371eb4aa38d683857385a9e8a843dd
Salomonrey <36483352+Salomonrey@users.noreply.github.com>
2019-09-02 10:05:30 +0600

    Sula

--- server/app/controllers/posts.js
+++ server/app/controllers/posts.js
@@ -25,7 +25,7 @@ module.exports.getPosts = async function(req, res) {
   res.status(200).json(post); 
   }); 
   } else { 
-  Post.find({}).sort({_id:-1}).exec(function(err, post) { 
+  Post.find({}).sort({$natural:-1}).exec(function(err, post) { 
   res.status(200).json(post); 
   }); 
   } 
