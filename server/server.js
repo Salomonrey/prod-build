96de9491e35925efd31081d47eea289721543491
Salomonrey <36483352+Salomonrey@users.noreply.github.com>
2019-09-19 15:17:55 +0600

    Sula

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ import { port } from "./config/dotenv";
 
 const app = require("./config/express");
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+ // res.setHeader('Content-Type', 'text/event-stream')
+ // res.setHeader('Cache-Control', 'no-cache')
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`🚀 Server ready at http://localhost:${port}`);
 });
