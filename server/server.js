6af7261f648f3492397fcf8ce71a49704c692af6
root <salixm@mail.ru>
2019-09-19 17:29:10 +0300

    dfdf

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ import { port } from "./config/dotenv";
 const app = require("./config/express");
 
 https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  ca: fs.readFileSync('chain.pem')
 }, app)
 .listen(port, function () {
-  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+  console.log('Example app listening on port 1338! Go to https://localhost:1338/')
 })
